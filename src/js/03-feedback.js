00450c4987d525052152f66f7e918f55341d94f9
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-03-04 15:22:10 +0300

    третье задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,24 @@
-// const FEEDBACK_FORM = 'feedback-form-state';
+import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-console.log(formEl);
+
+const inputEmailEl = document.querySelector('input');
+const inputMessageEl = document.querySelector('textarea');
+
+const FEEDBACK_FORM = 'feedback-form-state';
+
+formEl.addEventListener('input', formInput);
+formEl.addEventListener('submit', formSubmit);
+
+let formData = {};
+
+function formInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  JSON.parse(localStorage.getItem(FEEDBACK_FORM));
+  formEl.reset();
+}
